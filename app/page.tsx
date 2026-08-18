57fb1e4ff70f52caaf64652b2ad37a0e312b21ca
bidingurusmk <bidingurusmk@gmail.com>
2026-08-18 13:25:06 +0700

    master

--- app/page.tsx
+++ app/page.tsx
@@ -2,68 +2,23 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="flex flex-row justify-center gap-20 items-center h-full px-20">
+      <div className="w-1/4 flex justify-end">
+        <div className="border-10 w-50 h-50 border-white rounded-full overflow-hidden">
+          <Image
+            src="/images/alam.jpg"
+            alt="Gambar Alam"
+            width={500}
+            height={500}
+            className="rounded-full w-50 h-50"
+          />
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
+      <div className="w-3/4 text-white">
+        <h1>I am John Smith</h1>
+        <p>Frontend Developer</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt soluta enim beatae adipisci dicta atque expedita libero, cumque obcaecati ut unde at temporibus sapiente eligendi provident quisquam similique repellat laboriosam voluptatem magni repellendus aperiam nulla quos. Nostrum temporibus saepe modi itaque delectus mollitia ratione sequi. Nobis, exercitationem autem adipisci, consequatur mollitia earum ea sapiente quibusdam sed esse soluta. Praesentium quaerat veritatis repellendus error tempora similique maiores ut recusandae dolorem ipsam reprehenderit labore voluptate itaque a minus consequatur nostrum saepe porro numquam vitae, facere asperiores. Adipisci, ab veritatis ea nulla cupiditate eum placeat suscipit temporibus dignissimos dolorem. Cum aperiam expedita aut.</p>
+      </div>
     </div>
   );
 }
