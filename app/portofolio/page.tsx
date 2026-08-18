17fe319ede5aa8803725d85924fd5885e63ee6db
bidingurusmk <bidingurusmk@gmail.com>
2026-08-18 18:15:49 +0700

    rev tambahan

--- app/portofolio/page.tsx
+++ app/portofolio/page.tsx
@@ -5,20 +5,18 @@ const PortofolioPage = () => {
         <div className="px-40">
             <h1 className="text-3xl font-bold text-center">Portofolio</h1>
             <div className="grid grid-cols-4 gap-10">
-                {portofolioItems.map((item)=>(
-                    <div className="w-full">
-                    <div className="aspect-h-1 aspect-w-1 w-full 
+                {portofolioItems.map((item) => (
+                    <div key={item.id} className="w-full">
+                        <div className="aspect-h-1 aspect-w-1 w-full 
                     overflow-hidden rounded-md bg-gray-200 lg:aspect-none 
                     group-hover:opacity-75 lg:h-80">
-                        <Image src={item.image} 
-                    alt="Portofolio Image" className="rounded-lg w-full" width={500} height={500} />
+                            <Image src={item.image}
+                                alt="Portofolio Image" className="rounded-lg w-full" width={500} height={500} />
+                        </div>
+                        <h1>{item.name}</h1>
+                        <p>{item.description}</p>
                     </div>
-                    <h1>{item.name}</h1>
-                    <p>{item.description}</p>
-                </div>
                 ))}
-                
-
             </div>
         </div>
     );
