ca14ec5a9f50da517085082c9e2fe5c45735e8b8
bidingurusmk <bidingurusmk@gmail.com>
2026-08-18 15:15:03 +0700

    tambahan

---
+++ app/portofolio/page.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import { portofolioItems } from "@/data/portofolio";
+const PortofolioPage = () => {
+    return (
+        <div className="px-40">
+            <h1 className="text-3xl font-bold text-center">Portofolio</h1>
+            <div className="grid grid-cols-4 gap-10">
+                {portofolioItems.map((item)=>(
+                    <div className="w-full">
+                    <div className="aspect-h-1 aspect-w-1 w-full 
+                    overflow-hidden rounded-md bg-gray-200 lg:aspect-none 
+                    group-hover:opacity-75 lg:h-80">
+                        <Image src={item.image} 
+                    alt="Portofolio Image" className="rounded-lg w-full" width={500} height={500} />
+                    </div>
+                    <h1>{item.name}</h1>
+                    <p>{item.description}</p>
+                </div>
+                ))}
+                
+
+            </div>
+        </div>
+    );
+}
+export default PortofolioPage;
