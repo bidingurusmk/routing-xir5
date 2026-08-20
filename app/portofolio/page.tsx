d4ea4dec313a0cb7f751c11b291f02a87cfd7ec1
bidingurusmk <bidingurusmk@gmail.com>
2026-08-20 08:46:13 +0700

    rev tamabahn

--- app/portofolio/page.tsx
+++ app/portofolio/page.tsx
@@ -1,11 +1,18 @@
 import Image from "next/image";
 import { portofolioItems } from "@/data/portofolio";
-const PortofolioPage = () => {
+const PortofolioPage = async ({ searchParams }: 
+    { searchParams?: Promise<{ category?: string }> }) => {
+    const resolvedSearchParams = searchParams ? await searchParams : {};
+    const { category } = resolvedSearchParams;
+    const filteredItems = category
+        ? portofolioItems.filter(item => item.category === category)
+        : portofolioItems;
+
     return (
         <div className="px-40">
             <h1 className="text-3xl font-bold text-center">Portofolio</h1>
             <div className="grid grid-cols-4 gap-10">
-                {portofolioItems.map((item) => (
+                {filteredItems.map((item) => (
                     <div key={item.id} className="w-full">
                         <div className="aspect-h-1 aspect-w-1 w-full 
                     overflow-hidden rounded-md bg-gray-200 lg:aspect-none 
